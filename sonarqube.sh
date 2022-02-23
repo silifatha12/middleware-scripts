3e0cc66ce25cffd4d61a770f97d023aa508b66d7
Silifath <silifatha12@gmail.com>
2022-02-23 11:29:15 -0500

    updated sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -11,7 +11,6 @@ echo
 echo
 echo "Step 1: Java 11 installation"
 echo
-su - vagrant
 sudo yum update -y
 sudo yum install java-11-openjdk-devel -y
 sudo yum install java-11-openjdk -y
@@ -26,6 +25,7 @@ echo
 echo
 echo "Step 3: Extract packages"
 echo
+sudo yum install unzip -y
 sudo unzip /opt/sonarqube-9.3.0.51899.zip
 echo
 echo
@@ -38,16 +38,19 @@ echo
 echo
 echo "Step 5: Enable firewall with port 9000"
 echo
+sudo yum install firewalld -y
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
 sudo firewall-cmd --permanent --add-port=9000/tcp
 sudo firewall-cmd --reload
 echo
 echo
 echo "Connect to the SonarQube server through the browser using this Url and port."
 echo
-echo "http://192.168.56.33:9000"
+echo "http://192.168.56.40:9000"
 echo
 echo "Here are the default credentials for access:"
 echo
 echo "username: admin"
-echo "password: school1"
+echo "password: admin"
 echo
